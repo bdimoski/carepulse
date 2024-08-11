86028db9e591ac7aca0b25ff19d33b9a32f6236c
Bojan <bdimosky35@yahoo.com>
2024-08-11 12:34:55 +0200

    Add Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,39 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 MedLink
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        alt="patient"
+        width={1000}
+        height={1000}
+        className="size-img max-w-[50%]"
+      />
     </div>
   );
 }
