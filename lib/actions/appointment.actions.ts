61f201cd56cff12fd553c81e138500f57c1e1760
Bojan <bdimosky35@yahoo.com>
2024-08-15 18:09:35 +0200

    Add success page for new appointment submission

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -26,3 +26,17 @@ export const createAppointment = async (
     console.error("An error occurred while creating a new appointment:", error);
   }
 };
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const appointment = await databases.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
