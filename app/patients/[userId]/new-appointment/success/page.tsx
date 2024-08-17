899d8f8e0db5341b9f665be1ad02ccd668aed211
Bojan <bdimosky35@yahoo.com>
2024-08-17 16:18:44 +0200

    Add user view metrics for new appointment and registration pages

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -6,11 +6,19 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+import * as Sentry from '@sentry/nextjs'
+import { getUser } from '@/lib/actions/patient.actions';
+
 const Success = async ({ params: { userId }, searchParams }: SearchParamProps) => {
+    const user = await getUser(userId)
+
     const appointmentId = (searchParams?.appointmentId as string) || '';
     const appointment = await getAppointment(appointmentId);
 
     const doctor = Doctors.find((doctor) => doctor.name === appointment.primaryPhysician);
+
+    Sentry.metrics.set("user_view_appointment-success", user.name);
+
     return (
         <div className='flex h-screen max-h-screen px-[5%]'>
             <div className='success-img'>
--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,8 +3,12 @@ import { getUser } from '@/lib/actions/patient.actions'
 import Image from 'next/image'
 import React from 'react'
 
+import * as Sentry from '@sentry/nextjs'
+
 const Register = async ({ params: { userId } }: SearchParamProps) => {
     const user = await getUser(userId)
+
+    Sentry.metrics.set("user_view_register", user.name);
 
     return (
         <div className="flex h-screen max-h-screen">
