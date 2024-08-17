70b87088be428f4c09aad7b8645ae3fe4f77ccdc
Bojan <bdimosky35@yahoo.com>
2024-08-17 16:58:33 +0200

    Add SMS notifications

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -6,10 +6,12 @@ import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
+  messaging,
 } from "../appwrite.config";
 import { parseStringify } from "../utils";
 import { Appointment } from "@/types/appwrite.types";
 import { revalidatePath } from "next/cache";
+import { formatDateTime } from "@/lib/utils";
 
 //  CREATE APPOINTMENT
 export const createAppointment = async (
@@ -105,8 +107,38 @@ export const updateAppointment = async ({
       throw new Error("Appointment not found");
     }
 
+    const smsMessage = `
+      Hi, it's MedLink.
+     ${
+       type === "schedule"
+         ? `Your appointment has been scheduled for ${
+             formatDateTime(appointment.schedule).dateTime
+           } with Dr. ${appointment.primaryPhysician}`
+         : `We regret to inform you that your appointment has been cancelled for the following reason:
+          ${appointment.cancellationReason}`
+     }.
+    `;
+
+    await sendSMSNotification(userId, smsMessage);
+
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// SEND SMS NOTIFICATION
+export const sendSMSNotification = async (userId: string, content: string) => {
+  try {
+    const message = await messaging.createSms(
+      ID.unique(),
+      content,
+      [],
+      [userId]
+    );
+
+    return parseStringify(message);
   } catch (error) {
     console.log(error);
   }
